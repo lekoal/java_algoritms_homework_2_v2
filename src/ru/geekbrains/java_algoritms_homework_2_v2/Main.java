b1e8a0ddbd681c161dabfe5ae032b8cd207ba446
lekoal <coreglus@gmail.com>
2021-09-10 13:22:13 +0300

    Initial commit

---
+++ src/ru/geekbrains/java_algoritms_homework_2_v2/Main.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_algoritms_homework_2_v2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru/geekbrains/java_algoritms_homework_2_v2/MyArrayList.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.java_algoritms_homework_2_v2;
+
+public class MyArrayList {
+}
